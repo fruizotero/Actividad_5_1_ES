336f10b50af0a1471396f41b7bdd64e1ffc85e6e
Frank Edgar Ruiz Otero <a21frankero@ATEIS.LOCAL>
2024-01-08 18:05:43 +0100

    Clases para conexión y para traer datos

---
+++ scripts/funciones.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once "conexion.php";
+ function getRoles()
+ {
+
+     try {
+         $conn = getConnection();
+         $query = "SELECT * from rol";
+         $stmt = $conn->prepare($query);
+         $stmt->execute();
+         $values = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+         return $values;
+     } catch (PDOException $ex) {
+         echo $ex->getMessage();
+     }
+ }
+?>
