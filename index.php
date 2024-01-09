7070fabb2d65836f6ef706ace7b91404e71dcdcd
Frank Edgar Ruiz Otero <a21frankero@ATEIS.LOCAL>
2024-01-09 19:07:49 +0100

    Gestión del formulario y las alertas

--- index.php
+++ index.php
@@ -20,6 +20,7 @@
     $confirm_password = null;
     $role = null;
     $is_form_valid = true;
+    $is_user_register = false;
     $errors = [];
 
 
@@ -32,8 +33,8 @@
         $confirm_password = $_POST["confirm_password"];
         $role = $_POST["role"];
 
-        if (strcasecmp($password, $confirm_password) == 0) {
-            $errors[] = "Contraseñas no válidas";
+        if (strcasecmp($password, $confirm_password) !== 0) {
+            $errors[] = "Las contraseñas no coinciden";
             $is_form_valid = false;
         }
 
@@ -42,10 +43,13 @@
             $is_form_valid = false;
         }
 
-        foreach ($errors as $value) {
-            # code...
-            echo $value . "<br>";
+        if ($is_form_valid) {
+            $errors = [];
+            $is_user_register = register_user($email, $password, $role);
         }
+
+
+
 
         // $users = getUsers();
 
@@ -60,9 +64,9 @@
         // }
 
 
-        echo "<pre>";
-        print_r($_POST);
-        echo "</pre>";
+        // echo "<pre>";
+        // print_r($_POST);
+        // echo "</pre>";
     }
 
 
@@ -71,6 +75,12 @@
     ?>
 
     <div class="container mt-5">
+        <div class="row justify-content-end">
+            <div class="col-2">
+                <a href="pages/login.php">Login</a>
+
+            </div>
+        </div>
         <div class="row justify-content-center">
             <div class="col-md-6">
                 <h2>Registro de Usuario</h2>
@@ -107,8 +117,33 @@
                         </select>
                     </div>
                     <button type="submit" class="btn btn-primary">Registrar usuario</button>
+
                 </form>
+
+
             </div>
+            <section class="errors"></section>
+
+            <?php
+
+            if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+                if (!$is_form_valid) {
+
+                    foreach ($errors as  $error) {
+                        echo "<div class=\"alert alert-danger\" role=\"alert\">$error</div>";
+                    }
+                }
+
+                if ($is_user_register) {
+                    echo "<div class=\"alert alert-success\" role=\"alert\">El usuario se creo correctamente</div>";
+                } else {
+                    echo "<div class=\"alert alert-danger\" role=\"alert\">No se pudo crear el usuario</div>";
+                }
+            }
+
+
+            ?>
         </div>
     </div>
 
